e89c94e0f174db1614acc30b2e4b36731512e3bc
[Ibizus] <[hector.ldz@gmail.com]>
2023-01-16 10:29:49 +0100

    ej 3 array bi

--- ARRAY/Ej3ArrayBi.java
+++ ARRAY/Ej3ArrayBi.java
@@ -3,10 +3,29 @@ package ARRAY;
 public class Ej3ArrayBi {
     public static void main(String[] args) {
         
+        int[][] tabla = new int[4][5];
+
+        rellenarArrayBi(tabla);
 
 
 
 
-        
     }
+
+    /**
+     * Llena el array con numeros aleatorios de 0 a 100
+     * @param arrayBi
+     */
+    static void rellenarArrayBi(int[][] arrayBi)
+    {
+        for (int i = 0; i < arrayBi.length; i++)
+        {
+            for (int j = 0; j < arrayBi[i].length; j++)
+            {
+                arrayBi[i][j] = (int)(Math.random()*101);
+            }
+        }
+    }
+
+
 }
